cbd79606f5aadb373a661ad995a3980a8029e124
Yuliya Novik <Yuliya Novik>
2017-03-30 19:53:29 +0300

    Add selection and clear methods

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -32,12 +32,6 @@ namespace Mini_paint {
             painter.Draw(new Figure.Triangle(x, y, side));
         }
 
-        private void PanelOptionConfig(string str1, string str2, bool visible) {
-            labelSide1.Content = str1;
-            labelSide2.Content = str2;
-            side2.Visibility = (visible) ? Visibility.Visible : Visibility.Hidden;
-        }
-
         private bool CheckOption() {
             if (side1.Text == "" || (side2.IsVisible && side2.Text == "")) {
                 return false;
@@ -45,36 +39,36 @@ namespace Mini_paint {
             return true;
         }
 
-        private void ListFigure_SelectionChanged(object sender, SelectionChangedEventArgs e) {
-            switch (listFigure.SelectedIndex) {
-                  case 0: {
-                        PanelOptionConfig("Radius", "", false);
-                        break;
-                } case 1: {
-                        PanelOptionConfig("Side 1", "Side 2", true);
-                        break;
-                } case 2: {
-                        PanelOptionConfig("Side", "", false);
-                        break;
-                } case 3: {
-                        PanelOptionConfig("Side 1", "Side 2", true);
-                        break;
-                } case 4: {
-                        PanelOptionConfig("Side", "", false);
-                        break;
-                } case 5: {
-                        PanelOptionConfig("Side", "", false);
-                        break;
-                }
+        private void PanelOptionConfig(string str1, string str2, bool visible) {
+            labelSide1.Content = str1;
+            labelSide2.Content = str2;
+            side2.Visibility = (visible) ? Visibility.Visible : Visibility.Hidden;
+        }
 
-            }
+        private void DrawOnCanvas() {
+            Point point = Mouse.GetPosition(canvas);
+            CheckOption();
+            CreateDrawObject(point);
+        }
+
+        private void ButtonClear_Click(object sender, RoutedEventArgs e) {
+            painter.ClearPainter();
+        }
+
+        private void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
+            ClickBtn = true;
+            DrawOnCanvas();
+        }
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e) {
+            if (ClickBtn) DrawOnCanvas();
+        }
+
+        private void Canvas_MouseUp(object sender, MouseButtonEventArgs e) {
+            ClickBtn = false;
         }
 
         private void CreateDrawObject(Point point) {
-            if (point.X > 370) {
-                return;
-            }
-
             int sideLen1, sideLen2;
             if (!Int32.TryParse(side1.Text, out sideLen1)) {
                 return;
@@ -107,50 +101,73 @@ namespace Mini_paint {
             }
         }
 
-        private void CheckMouseOver(Point point) {
-            if (point.X >= 370 || point.X <= 3 || point.Y >= 450 || point.Y <= 10) {
-                ClickBtn = false;
+        private void ListFigure_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            switch (listFigure.SelectedIndex) {
+                  case 0: {
+                        PanelOptionConfig("Radius:", "", false);
+                        break;
+                } case 1: {
+                        PanelOptionConfig("Side 1:", "Side 2:", true);
+                        break;
+                } case 2: {
+                        PanelOptionConfig("Side:", "", false);
+                        break;
+                } case 3: {
+                        PanelOptionConfig("Side 1:", "Side 2:", true);
+                        break;
+                } case 4: {
+                        PanelOptionConfig("Side:", "", false);
+                        break;
+                } case 5: {
+                        PanelOptionConfig("Side:", "", false);
+                        break;
+                }
+
             }
         }
 
-        private void DrawOnCanvas() {
-            Point point = Mouse.GetPosition(canvas);
-            CheckMouseOver(point);
-            CheckOption();
-            CreateDrawObject(point);
-        }
-
-        private void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
-            ClickBtn = true;
-            DrawOnCanvas();
-        }
-
-        private void Canvas_MouseMove(object sender, MouseEventArgs e) {
-            if (ClickBtn) DrawOnCanvas();
-        }
-
-        private void Canvas_MouseUp(object sender, MouseButtonEventArgs e) {
-            ClickBtn = false;
-        }
-
-        private void ListColor_SelectionChanged(object sender, SelectionChangedEventArgs e) {
-            switch (listColor.SelectedIndex) {
+        private void ListFillColor_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            switch (listFillColor.SelectedIndex) {
                   case 0: {
-                        painter.SetBrush(255, 255, 255);
+                        painter.SetBrushFill(255, 255, 255);
                         break;
                 } case 1: {
-                        painter.SetBrush(0, 0, 0);
+                        painter.SetBrushFill(0, 0, 0);
                         break;
                 } case 2: {
-                        painter.SetBrush(255, 0, 0);
+                        painter.SetBrushFill(255, 0, 0);
                         break;
                 } case 3: {
-                        painter.SetBrush(0, 255, 0);
+                        painter.SetBrushFill(0, 255, 0);
                         break;
                 } case 4: {
-                        painter.SetBrush(0, 0, 255);
+                        painter.SetBrushFill(0, 0, 255);
                         break;
                 }
+            }
+        }
+        private void ListStrokeColor_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            switch (listStrokeColor.SelectedIndex) {
+                case 0: {
+                        painter.SetBrushStroke(255, 255, 255);
+                        break;
+                    }
+                case 1: {
+                        painter.SetBrushStroke(0, 0, 0);
+                        break;
+                    }
+                case 2: {
+                        painter.SetBrushStroke(255, 0, 0);
+                        break;
+                    }
+                case 3: {
+                        painter.SetBrushStroke(0, 255, 0);
+                        break;
+                    }
+                case 4: {
+                        painter.SetBrushStroke(0, 0, 255);
+                        break;
+                    }
             }
         }
     }
